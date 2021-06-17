369f0c1190d2d4e958fcddd48b5db9b2302ab3d9
ponnarasi <85145307+ponnarasik@users.noreply.github.com>
2021-06-17 14:38:34 +0000

    save

--- index.js
+++ index.js
@@ -4,8 +4,23 @@
 
 console.log(add(1, 1)); */
 
-let f1 = function(str) {
+
+
+
+
+/*let f1 = function(str) {
    return str + str;
 };
 f1("abcd");
-let f2 = f1;
+let f2 = f1;*/
+
+let nums = [3.14, 42, 4811];
+
+let timesTwo = function (n) {
+   return n*2;
+};
+
+let doubled = nums.map(timesTwo);
+
+console.log(nums);
+console.log(doubled);
